c9dc40791e453285fc630eeddf730891e41ecc41
jbarker256 <Joseph.Barker@a2-g.com>
2025-10-02 10:10:06 -0400

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,7 +5,7 @@ mkdir -p /home/ec2-user/app
 cd /home/ec2-user/app
 
 if [ -f docker-compose.yml ]; then
-  docker compose down || true
+  /usr/local/bin/docker-compose down || true
 fi
 
 echo "Stop script completed"
