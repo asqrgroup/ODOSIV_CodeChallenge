7d6416cb39b2897ed12aa6172f69cf2f592b8a92
jbarker256 <Joseph.Barker@a2-g.com>
2025-10-02 16:11:45 -0400

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd /home/ec2-user/app
-docker compose -f /home/ec2-user/app/docker-compose.yml down || true
+/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
 echo "Containers stopped"
