31a1a8fce58167a07a18d8220815f1349264cb46
jbarker256 <Joseph.Barker@a2-g.com>
2025-10-02 16:11:11 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd /home/ec2-user/app
-docker compose -f /home/ec2-user/app/docker-compose.yml down || true
-docker compose -f /home/ec2-user/app/docker-compose.yml up -d --build
+/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
+/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml up -d --build
 echo "App container started"
