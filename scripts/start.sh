bb36b74cb4882deca80d3f6518e9e3a5f4848c6a
gowthampal <81435098+gowthampal@users.noreply.github.com>
2025-10-03 16:09:43 -0400

    Update start.sh by Gowtham

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
-cd /home/ec2-user/app
-/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
+echo "Switching to app directory"
+cd /home/ec2-user/app 
+echo "Inside APP DIR"
+#/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
+#/usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml up -d --build
+if [ ! -f "docker-compose.yml" ]; then
+  echo "ERROR: docker-compose.yml not found in APP DIR"
+  exit 1
+fi
+/usr/bin/docker compose -f docker-compose.yml down || true
 /usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml up -d --build
 echo "App container started"
