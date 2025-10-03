993786e5e18695f4973c252e2eebf221bfd7fa90
jbarker256 <Joseph.Barker@a2-g.com>
2025-10-02 20:20:40 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,3 @@ cd /home/ec2-user/app
 /usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
 /usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml up -d --build
 echo "App container started"
-
-echo "Running docker compose up..."
-/usr/bin/docker compose -f docker-compose.yml up -d --build
-
-echo "===== DEPLOYMENT COMPLETE ====="
