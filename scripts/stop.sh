a217882d43269a26cc8216469c4ac5c78ad50c20
jbarker256 <Joseph.Barker@a2-g.com>
2025-10-03 21:59:41 -0400

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,6 +3,4 @@ stop.sh
 set -e
 cd /home/ec2-user/app
 /usr/bin/docker compose -f /home/ec2-user/app/docker-compose.yml down || true
-#clean up old files so permissions can be set correctly
-rm -rf /home/ec2-user/app/*
 echo "Containers stopped"
